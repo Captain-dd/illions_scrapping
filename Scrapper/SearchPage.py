12b50e097dd99bd785793310bbca98a459c7e512
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2022-11-04 17:47:42 +0530

    new data

--- Scrapper/SearchPage.py
+++ Scrapper/SearchPage.py
@@ -11,19 +11,18 @@ class TestSearchPage:
 
     def test_perform_search(self):
 
-        columns = ['name','address' ,'mobile_number' ]
+        columns = ['name', 'address', 'mobile_number']
 
-        df = pd.DataFrame(columns = columns)
+        df = pd.DataFrame(columns=columns)
 
         cu = CommonUtilites()
         cu.click_element(element_locator=Spl.provider_city_radioButton)
-        cu.send_text(element_locator=Spl.city_field, text= 'New York')
+        cu.send_text(element_locator=Spl.city_field, text='New York')
         cu.click_element(element_locator=Spl.search_button)
         time.sleep(4)
         cu.scroll_down()
         doctor_name_lst = cu.get_multiple_button(element_locator=Rpl.doctor_name)
         doctor_details = cu.get_multiple_button(element_locator=Rpl.doctor_address)
-
 
         print("doctor name")
         for i in range(len(doctor_name_lst)):
@@ -34,21 +33,16 @@ class TestSearchPage:
             except:
                 print(i)
 
-
         for i in range(len(doctor_details)):
             try:
-                temp  = doctor_details[i].text
-                if i%2==0:
-                    df.at[i//2, 'address'] = temp
+                temp = doctor_details[i].text
+                if i % 2 == 0:
+                    df.at[i // 2, 'address'] = temp
 
                 else:
-                    df.at[i//2, 'mobile_number'] = temp
+                    df.at[i // 2, 'mobile_number'] = temp
 
             except:
                 pass
 
-
-        print(len(doctor_name_lst),len(doctor_details))
-
-
-        df.to_csv(f'jindal.csv',index=False)
+        df.to_csv(f'jindal.csv', index=False)
